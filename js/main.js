195b6cb3bca5a769e7229a7cf2c7f04c789e7951
Federico <federicobartoli@me.com>
2020-04-07 17:38:13 +0200

    Push

--- js/main.js
+++ js/main.js
@@ -1,34 +1,66 @@
 $(document).ready(function () {
 
-     var settings = {
-       "url": "http://157.230.17.132:4001/sales",
-       "method": "GET",
-       "timeout": 0,
-     };
-
-
      leMieVendite = {
 
-     }
+     };
 
-     $.ajax(settings).done(function (response) {
+     var mesiDiVendita = [];
+     var datiVenditeMxM = [];
+
+     $.ajax({
+          url: "http://157.230.17.132:4001/sales",
+          method: 'GET',
+          success: function(response){
             for (var i = 0; i < response.length; i++) {
                 var venditaSingolaGenerale = response[i];
                 var dataVendita = venditaSingolaGenerale.date;
                 var tempo = moment(dataVendita, "DD-MM-YYYY"); //uso moment
                 var mese = tempo.format('MMMM');
                 var venditeMonetarie = venditaSingolaGenerale.amount
-                var venditeMonetarieCorrette = parseInt(venditeMonetarie);
-                console.log(venditeMonetarieCorrette);
+                var venditeMonetarieCorrette = parseInt(venditeMonetarie); //tolgo gli apici '' in un numero
                 if (leMieVendite[mese] === undefined) {
                     leMieVendite[mese] = 0;
                }
                leMieVendite[mese] += venditeMonetarieCorrette;
-               console.log(leMieVendite);
-
                }
+
+               for (var chiave in leMieVendite) {
+                    mesiDiVendita.push(chiave);
+                    datiVenditeMxM.push(leMieVendite[chiave])
+               }
+
+               console.log(mesiDiVendita);
+               console.log(datiVenditeMxM);
+          },
+          error: function(){
+               alert('errore');
+          }
      });
-     // console.log(mesiDiVendita);
-     // console.log(moment.months());
+
+
+
+     //IL MIO GRAFICO
+     var myChart = new Chart($('#grafico-delle-vendite'), {
+         type: 'line',
+         data: {
+             labels: mesiDiVendita,
+             datasets: [{
+                  label: 'Vendite mensili',
+                 backgroundColor: [
+                     'rgba(255, 99, 132, 0.2)',
+                 ],
+                 borderColor: [
+                     'rgba(255, 99, 132, 1)',
+                     'rgba(54, 162, 235, 1)',
+                     'rgba(255, 206, 86, 1)',
+                     'rgba(75, 192, 192, 1)',
+                     'rgba(153, 102, 255, 1)',
+                     'rgba(255, 159, 64, 1)'
+                 ],
+                 data: datiVenditeMxM
+             }]
+         },
+    });
+
 
 });
